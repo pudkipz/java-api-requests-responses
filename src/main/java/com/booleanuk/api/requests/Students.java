f835e264e3970823186233aabe4c8bc4c6a4787b
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-17 13:59:05 +0100

    Implement mappings for Students

--- src/main/java/com/booleanuk/api/requests/Students.java
+++ src/main/java/com/booleanuk/api/requests/Students.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("students")
+@RequestMapping("/students")
 public class Students {
-    private List<Student> students = new ArrayList<>(){{
+    private List<Student> students = new ArrayList<>() {{
         add(new Student("Nathan", "King"));
         add(new Student("Dave", "Ames"));
     }};
@@ -23,7 +23,50 @@ public class Students {
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Student> getAll() {
         return this.students;
+    }
+
+    @GetMapping("/{firstName}")
+    @ResponseStatus(HttpStatus.OK)
+    public Student getStudent(@PathVariable(name = "firstName") String firstName) {
+        for (Student s : students) {
+            if (s.getFirstName().equals(firstName)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    @PutMapping("/{firstName}")
+    @ResponseStatus(HttpStatus.OK)
+    public Student updateStudent(
+            @PathVariable(name = "firstName") String firstName,
+            @RequestBody Student newStudent) {
+        for (int i=0; i<students.size(); i++) {
+            if (students.get(i).getFirstName().equals(firstName)) {
+                students.set(i, newStudent);
+                return students.get(i);
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{firstName}")
+    @ResponseStatus(HttpStatus.OK)
+    public Student deleteStudent(@PathVariable(name = "firstName") String firstName) {
+        boolean foundStudent = false;
+        int i;
+        for (i = 0; i < students.size(); i++) {
+            if (students.get(i).getFirstName().equals(firstName)) {
+                foundStudent = true;
+                break;
+            }
+        }
+        if (foundStudent) {
+            return students.remove(i);
+        }
+        return null;
     }
 }
